e1060d8a349feba86078be5b30ed6601ebf3f993
ice1224 <mariuszkobiela1224@gmail.com>
2019-05-10 18:50:21 +0200

    Add track search

--- app/src/main/java/com/kobiela/urbangame/TrackChoiceActivity.java
+++ app/src/main/java/com/kobiela/urbangame/TrackChoiceActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,18 +32,40 @@ public class TrackChoiceActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    private EditText etSearchTrack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_choice);
 
+        etSearchTrack = findViewById(R.id.et_search_track);
+        etSearchTrack.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                readData(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        readData();
+        readData("");
     }
 
-    private void readData() {
+    private void readData(final String toSearch) {
+        trackList.clear();
+        idsList.clear();
         FirebaseFirestore.getInstance().collection("tracksCollection")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -53,8 +78,10 @@ public class TrackChoiceActivity extends AppCompatActivity {
                                 String title = document.get("TITLE").toString();
                                 String description = document.get("DESCRIPTION").toString();
                                 String location = document.get("LOCATION").toString();
-                                trackList.add(new Track(title,description,location));
-                                idsList.add(document.getId());
+                                if(title.contains(toSearch) || location.contains(toSearch)){
+                                    trackList.add(new Track(title,description,location));
+                                    idsList.add(document.getId());
+                                }
                             }
                             Toast.makeText(TrackChoiceActivity.this, trackList.size() + " " + idsList.size(), Toast.LENGTH_LONG).show();
                             initializeRecyclerView();
